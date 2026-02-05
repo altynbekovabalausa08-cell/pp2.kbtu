d7f39d35011f10b9b8294d381f749c040f87c1ea
altynbekovabalausa08-cell <altynbekovabalausa08@gmal.com>
2026-02-06 00:37:29 +0500

    Add Practice 2: boolean,if-else,and loops

---
+++ Practice 2/boolean/boolean_comparison.py
@@ -0,0 +1,10 @@
+a = 1
+b = 7
+c = "hello"
+d = "world"
+
+print(a == b)      
+print(a != b)     
+print(c == d)    
+print(c != d)     
+print((a == b) and (c != d))
---
+++ Practice 2/boolean/boolean_operators.py
@@ -0,0 +1,9 @@
+x = True
+y = False
+
+print(x and y)  
+print(x or y)   
+print(not x)   
+
+num = 10
+print(num > 5 and num < 15) 
